6d2dd17f5b76dbcafe69f9fbff4d12204cd004ec
Adriel Casellas <adrielcasellas@gmail.com>
2018-05-14 09:48:49 -0400

    Python Scraper Stable

--- Python/ScrapeModelInfo.py
+++ Python/ScrapeModelInfo.py
@@ -1,20 +1,38 @@
 # install chromedriver from https://chromedriver.storage.googleapis.com/index.html?path=2.38/
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#URL = "https://www.autotrader.com/cars-for-sale/vehicledetails.xhtml?listingId=484656454&zip=84604&referrer=%2Fcars-for-sale%2Fsearchresults.xhtml%3Fzip%3D84604%26startYear%3D1981%26sortBy%3Drelevance%26firstRecord%3D0%26endYear%3D2019%26modelCodeList%3DWRANGLER%26makeCodeList%3DJEEP%26searchRadius%3D25&startYear=1981&numRecords=25&firstRecord=0&endYear=2019&modelCodeList=WRANGLER&makeCodeList=JEEP&searchRadius=25"
+
 
 class AutoTraderSession():
     def __init__(self):
         self.browser = webdriver.Chrome(r'chromedriver')
 
-    def getData(self, url, CSSselector):
+    def getData(self, url, button, dataTable):
         self.browser.get(url)
-        self.browser.find_element_by_css_selector(CSSselector).click()
-        return self.browser.page_source
+        self.browser.find_element_by_css_selector(button).click()
 
-def main(AutoURL):
+        try:
+            element = WebDriverWait(self.browser, 5).until(
+            EC.visibility_of_element_located((By.ID, "484656454-pane-2"))
+            )
+
+        finally:
+            elems = self.browser.find_element_by_xpath(dataTable)
+
+        return(element.text)
+
+def scrape_button(AutoURL):
     Session = AutoTraderSession()
     CSSSelectorButton = ".active+ li a , .active+ li span" # This is the model info button
+    CSSDataTable = '//*[contains(concat( " ", @class, " " ), concat( " ", "padding-horizontal-lg", " " ))]'
 
-    return Session.getData(url = AutoURL, CSSselector = CSSSelectorButton)
+    data = Session.getData(url = AutoURL, button = CSSSelectorButton, dataTable = CSSDataTable)
     Session.browser.quit()
+    return data
 
-    
+#if __name__ == '__main__':
+#    scrape_button(URL)
