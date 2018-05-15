d7a6f1d9cc1d539b2b179b105dd9289aa1e24703
Adriel Casellas <adrielcasellas@gmail.com>
2018-05-15 16:45:59 -0400

    paths

--- R/Functions/py_button_scrape.R
+++ R/Functions/py_button_scrape.R
@@ -1,3 +1,9 @@
+library(tidyverse)
+library(rvest) # read_html functions and the like
+library(httr)
+library(pbapply) # For progress bars in queries
+library(parallel) # For parallel processing
+library(pbapply)
 library(reticulate)
 
 listing_url <- "https://www.autotrader.com/cars-for-sale/vehicledetails.xhtml?listingId=484656454&zip=84604&referrer=%2Fcars-for-sale%2Fsearchresults.xhtml%3Fzip%3D84604%26startYear%3D1981%26sortBy%3Drelevance%26firstRecord%3D0%26endYear%3D2019%26modelCodeList%3DWRANGLER%26makeCodeList%3DJEEP%26searchRadius%3D25&startYear=1981&numRecords=25&firstRecord=0&endYear=2019&modelCodeList=WRANGLER&makeCodeList=JEEP&searchRadius=25"
